efeae6a863714c2ece485384c266f44d1c03e23d
龙邦 <a13535@foxmail.com>
2025-10-13 00:24:07 +0800

    新增多表支持

---
+++ idb.ts
@@ -0,0 +1,118 @@
+import Itb from './itb';
+
+class Idb{
+    protected db_:unknown;
+    protected tb_:unknown = {}; //多表
+    db_name:string; //库名称
+    version:number; //版本号
+    constructor(db_name:string,version:number){
+        this.db_name = db_name;
+        this.version = version;
+    }
+    //table为表名称，primary_key为主键名，index为索引名(索引值可以是，字符串、对象、数组、数组对象。假设index是 {'name':true} 表示创建一个名称为name的索引，true表示值不允许重复，false表示允许重复)
+    //创建多个表的示例：table=[['tb1','id','name'],['tb2','id','name']]（数组的第一个值为表名称，第二个为主键名，第三个为索引名）；primary_key=null；index=null
+    async open(table,primary_key,index){
+        return new Promise((resolve, reject) => {
+            const request = indexedDB.open(this.db_name,this.version);
+            //版本号变化或首次创建时触发
+            request.onupgradeneeded = (event) => {
+                this.db_ = event.target.result;
+                if(Array.isArray(table[0])){
+                    table.forEach(v=>{
+                        this.createTable_(v[0],v[1],v[2]);
+                    });
+                }else if(Array.isArray(table)){
+                    this.createTable_(table[0],table[1],table[2]);
+                }else if(typeof table === 'string'){
+                    this.createTable_(table,primary_key,index);
+                }
+            }
+            //成功回调
+            request.onsuccess = (event) => {
+                this.db_ = event.target.result;
+                let i=0;
+                for(i;i<this.db_.objectStoreNames.length;i++){
+                    this.tb_[this.db_.objectStoreNames[i]] = new Itb(this.db_,this.db_.objectStoreNames[i]);
+                }
+                if(i==1){
+                    resolve(this.tb_[this.db_.objectStoreNames[0]]);
+                }else{
+                    resolve(this.tb_);
+                }
+            }
+            //错误回调
+            request.onerror = (event) => {
+                reject(event.target.errorCode);
+            }
+        });
+    }
+    //创建表，table_name表名称；primary_key主键名称；index索引名称
+    protected createTable_(table_name,primary_key,index){
+        //检查旧版本中是否已有需要的对象存储（表）
+        if (!this.db_.objectStoreNames.contains(table_name)) {
+            //创建表（如果不设置keyPath(主键)，可autoIncrement=true(自动管理键)）
+            const store = this.db_.createObjectStore(table_name, {
+                keyPath: primary_key || null,
+                autoIncrement: !primary_key
+            });
+            //如果不建其它索引，默认只能搜索主键（即从主键取值）
+            //createIndex参数1:索引的名称；参数2:关联的字段名称；参数3:unique 为 true 不允许重复的值
+            if(index){
+                //如果index只是一个字符串，根据该字符串创建允许重复的索引
+                if(typeof index === 'string'){
+                    store.createIndex(index, index, { unique: false });
+                }else{
+                    //如果index是一个数组，并且数组的值只是一个字符串，根据该字符串创建允许重复的索引
+                    if(typeof index[0] === 'string'){
+                        index.forEach(v=>{
+                            if(typeof v === 'string') {
+                                store.createIndex(v, v, { unique: false });
+                            }else{
+                                console.error(v,'not string');
+                                return -1;
+                            }
+                        });
+                    }else{
+                        //如果index是一个数组，并且数组的值是一个对象，根据对象的key创建索引名，对象的值为unique的值，false表示允许重复，true表示不允许重复
+                        if(Array.isArray(index)){
+                            index.forEach(v=>{
+                                if(typeof Object.keys(v)[0] === 'string') {
+                                    store.createIndex(Object.keys(v)[0], Object.keys(v)[0], { unique: Object.values(v)[0] });
+                                }else{
+                                    console.error(typeof Object.keys(v)[0],'not string');
+                                    return -1;
+                                }
+                            });
+                        }else{
+                            //如果index是一个对象，根据对象的key创建索引名，对象的值为unique的值
+                            if(typeof Object.keys(index)[0] === 'string') {
+                                store.createIndex(Object.keys(index)[0], Object.keys(index)[0], { unique: Object.values(index)[0] });
+                            }else{
+                                console.error(Object.keys(index)[0],'not string');
+                                return -1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //删除库
+    protected delDB(){
+        return new Promise((resolve, reject) => {
+            const request = indexedDB.deleteDatabase(this.db_name);
+            request.onsuccess = (event)=>{
+                resolve(event.target.result);
+            }
+            request.onerror = (event)=>{
+                reject(event.target.errorCode);
+            };
+        });
+    }
+    //选择要操作的存储对象
+    /* use(tb_name){
+        return this.tb_[tb_name];
+    } */
+};
+
+export default Idb;
---
+++ itb.ts
@@ -0,0 +1,194 @@
+class Itb{
+    protected db_:unknown; //存储库
+    protected store_:unknown; //存储对象
+    protected model_:number; //当前模式
+    protected tb_name:string; //存储对象的名称
+    constructor(db:unknown,tb_name:string){
+        this.db_ = db;
+        this.tb_name = tb_name;
+        this.model_ = 0;
+    }
+    //只读
+    protected read_(){
+        this.model_ = 1;
+        //新建一个事务（参数1:是将要处理的表，可以是数组。参数2:处理模式（默认readonly只读 或 readwrite读写））
+        this.store_ = this.db_.transaction([this.tb_name],'readonly').objectStore([this.tb_name]);
+    };
+    //可写
+    protected write_(){
+        this.model_ = 2;
+        this.store_ = this.db_.transaction([this.tb_name], 'readwrite').objectStore([this.tb_name]);
+    };
+    //删除表
+    protected async delTable(){
+        return new Promise((resolve, reject) => {
+            const request = indexedDB.open(this.db_['name'],this.db_['version']);
+            request.onupgradeneeded = (event)=>{
+                event.target.result.deleteObjectStore(this.tb_name);
+            }
+            request.onsuccess = (event)=>{
+                event.target.result.close();
+                resolve(event.target.result);
+            }
+            request.onerror = (event)=>{
+                reject(event.target.errorCode);
+            };
+        });
+    };
+    //清空表
+    async clear(){
+        return new Promise((resolve, reject) => {
+            if(this.model_ != 2){this.write_();}
+            let request = this.store_.clear();
+            request.onsuccess = (event)=>{
+                resolve(event.target.result);
+            };
+            request.onerror = (event)=>{
+                reject(event.target.error);
+            };
+        });
+    }
+    //删除数据
+    async del(primary_key){
+        return new Promise((resolve, reject) => {
+            if(this.model_ != 2){this.write_();}
+            let request = this.store_.delete(primary_key);
+            request.onsuccess = (event)=>{
+                resolve(event.target.result);
+            };
+            request.onerror = (event)=>{
+                reject(event.target.error);
+            };
+        });
+    }
+    //新增数据
+    async add(data){
+        return new Promise((resolve, reject) => {
+            if(this.model_ != 2){this.write_();}
+            let request = this.store_.add(data);
+            request.onsuccess = (event)=>{
+                resolve(event.target.result);
+            }
+            request.onerror = (event)=>{
+                reject(event.target.error);
+            }
+        });
+    }
+    //更新数据
+    async put(primary_key,data){
+        return new Promise((resolve, reject) => {
+            if(this.model_ != 2){this.write_();}
+            let request = this.store_.put(data,primary_key);
+            request.onsuccess = (event)=>{
+                resolve(event.target.result);
+            }
+            request.onerror = (event)=>{
+                reject(event.target.error);
+            }
+        });
+    }
+    //只有 k 表示通过主键获取数据
+    //如果有 v 表示通过索引获取数据，k=索引名，v=索引值（前提是创建了索引）
+    async get(k,v){
+        return new Promise((resolve, reject) => {
+            if(this.model_ != 1){this.read_();}
+            let request = '';
+            if(v){
+                const index = this.store_.index(k);
+                request = index.get(v);
+                
+            }else{
+                request = this.store_.get(k);
+            }
+            request.onsuccess = (event)=>{
+                resolve(event.target.result);
+            };
+            request.onerror = (event)=>{
+                reject(event.target.error);
+            };
+        });
+    }
+    //通过索引查找数据。index:索引名；只有 v1 表示模糊查找；如果有 v2 就是范围查询
+    async getF(index,v1,v2){
+        return new Promise((resolve, reject) => {
+            if(this.model_ != 1){this.read_();}
+            let data = [];
+            let request = {};
+            let cursor = {};
+            if(index){
+                const ii = this.store_.index(index);
+                if(v2){ //游标范围查询（适用于前缀匹配），从v1到v2之间的数据
+                    request = ii.openCursor(IDBKeyRange.bound(v1, v2)); //openCursor参数1:查询范围；参数2:遍历方向(默认为下一个，prev为上一个)(两个参数都空为默认，表示所有数据升序遍历)
+                    request.onsuccess = (event)=>{
+                        cursor = event.target.result;
+                        if (cursor) {
+                            data.push(cursor.value);
+                            cursor.continue();
+                        }
+                        resolve(data);
+                    };
+                }else{ //模糊查找
+                    const regex = new RegExp(v1, 'i'); //'i'表示不区分大小写
+                    request = ii.openCursor();
+                    request.onsuccess = (event)=>{
+                        cursor = event.target.result;
+                        if(cursor){
+                            if(regex.test(cursor.value[index])){
+                                data.push(cursor.value);
+                            }
+                            cursor.continue();
+                        }
+                        resolve(data);
+                    };
+                }
+            }else{ //游标方式获取所有数据
+                request = this.store_.openCursor();
+                request.onsuccess = (event)=>{
+                    cursor = event.target.result;
+                    if(cursor){
+                        data.push(cursor.value);
+                        cursor.continue();
+                    }
+                    resolve(data);
+                };
+            }
+            request.onerror = (event)=>{
+                reject(event.target.error);
+            };
+        });
+    }
+    //获取全部数据
+    async getAll(){
+        return new Promise((resolve, reject) => {
+            if(this.model_ != 1){this.read_();}
+            const request = this.store_.getAll();
+            request.onsuccess = (event)=>{
+                resolve(event.target.result);
+            };
+            request.onerror = (event)=>{
+                reject(event.target.error);
+            };
+        });
+    }
+    //获取全部主键
+    async getAllK(){
+        return new Promise((resolve, reject) => {
+            if(this.model_ != 1){this.read_();}
+            const request = this.store_.openKeyCursor();
+            let data = [];
+            request.onsuccess = (event)=>{
+                const cursor = event.target.result;
+                if (cursor) {
+                    data.push(cursor.primaryKey);
+                    cursor.continue();
+                }
+                resolve(data);
+            };
+            request.onerror = (event)=>{
+                reject(event.target.error);
+            };
+        });
+    }
+}
+
+export default Itb;
